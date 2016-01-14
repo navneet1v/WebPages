ff38a52395345f3c3a6f7b811d34075d7f47eb58
navneet1v <vermanavneet003@gmail.com>
2016-01-15 01:51:55 +0530

    First Commit

---
+++ myProgram.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int i = 0;
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cout<<"Yes it is working";
+		cout<<"Also who are you";
+	}
+	return 0;
+}
